65b6662886a3f83de8a7fd52386d5b8843cdd182
Rick <rickwillcox90@gmail.com>
2021-10-22 22:51:31 +1000

    Added All in game sounds we can use so far (including ones that we will test)
    Menu sounds functional. https://trello.com/c/9YBeipDM

--- Scenes/CreateAccountScreen.gd
+++ Scenes/CreateAccountScreen.gd
@@ -8,16 +8,22 @@ onready var back_button = get_node("Background/VBoxContainer/BackButton")
 onready var login_screen = get_parent().get_node("LoginScreen")
 onready var account_created_message_screen = get_node("AccountCreatedMessageScreen")
 onready var create_account_screen = self
+onready var menu_pressed_sound = get_node("../../MenuSounds/MenuPressSound")
+onready var menu_failed_sound = get_node("../../MenuSounds/MenuFailedSound")
+onready var menu_login_succeeded_sound = get_node("../../MenuSounds/MenuLoginSucceededSound")
 
 func _ready():
 	self.visible = false
 
 func _on_CreateAccountButton_pressed():
 	if username_input.text == "" or userpassword_input.text == "" or userpasswordrepeat_input.text == "":
+		menu_failed_sound.play()		
 		print("Please provide valid userID and password")
 	elif userpassword_input.text != userpasswordrepeat_input.text:
+		menu_failed_sound.play()	
 		print("Passwords do not match")
 	else:
+		menu_pressed_sound.play()
 		back_button.disabled = true
 		create_account_button.disabled = true
 		var username = username_input.get_text()
@@ -27,6 +33,8 @@ func _on_CreateAccountButton_pressed():
 		
 
 func _on_Back_Button_pressed():
+	menu_pressed_sound.play()
+	menu_pressed_sound.play()
 	self.visible = false
 	login_screen.visible = true
 
--- Scenes/LoginScreen.gd
+++ Scenes/LoginScreen.gd
@@ -5,6 +5,9 @@ onready var userpassword_input = get_node("Background/VBoxContainer/Password")
 onready var login_button = get_node("Background/VBoxContainer/Loginbutton")
 onready var create_account_screen = get_parent().get_node("CreateAccountScreen")
 onready var login_failed_message_screen = get_node("LoginFailedMessageScreen")
+onready var menu_pressed_sound = get_node("../../MenuSounds/MenuPressSound")
+onready var menu_failed_sound = get_node("../../MenuSounds/MenuFailedSound")
+onready var menu_login_succeeded_sound = get_node("../../MenuSounds/MenuLoginSucceededSound")
 
 var local = true
 export var auto_login = false
@@ -15,6 +18,7 @@ func _ready():
 	
 func _on_Login_pressed():
 	if username_input.text == "" or userpassword_input.text == "":
+		menu_failed_sound.play()
 		#popup and stop
 		print("Please provide valid userID and password")
 	else:
@@ -24,15 +28,18 @@ func _on_Login_pressed():
 		var password = userpassword_input.get_text()
 		print("Attempting to login")
 		Gateway.ConnectToServer(username, password, false)
+		menu_pressed_sound.play()
 
 
 func _on_CreateAccount_pressed():
 	self.visible = false
 	create_account_screen.visible = true
+	menu_pressed_sound.play()
 	
 
 
 func _on_IPButton_pressed():
+	menu_pressed_sound.play()
 	if local:
 		local = false
 		get_node("Background/VBoxContainer/IPButton/IPText").text = "Online"
