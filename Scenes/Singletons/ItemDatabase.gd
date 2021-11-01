494eca458c7472067d5501a409c37ced676e53f2
rick <rickwillcox90@gmail.com>
2021-11-01 22:46:12 +1000

    added some more checks and pulled equipcategories from database

--- Scenes/Singletons/ItemDatabase.gd
+++ Scenes/Singletons/ItemDatabase.gd
@@ -5,16 +5,24 @@ var all_item_data : Array = []
 #[{head:1}, {chest:2}, {hands:3}, {legs:4}, {feet:5}, {main_hand:6}, {off_hand:7}, {ring:8}, {amulet:9}, {not_an_item:0}]
 var item_categories : Array = []
 
+var equip_slots : Array = []
+
 func GetItemID(file_name):
 	for item in all_item_data:
 		if item["file_name"] == file_name:
-			return item["item_id"]
+			return int(item["item_id"])
 			
 func GetItemCategory(file_name):
 	for item in all_item_data:
 		if item["file_name"] == file_name:
-			return item["item_category_id"]
+			return int(item["item_category_id"])
 
 func CreateItemCategoriesList(item_categories_data):
 	for category in item_categories_data:
-		item_categories.append({category["item_category"] : category["item_category_id"]})
+		item_categories.append({category["item_category"] : int(category["item_category_id"])})
+
+func CreateEquipSlots(equip_slots_data):
+	for equip in equip_slots_data:
+		equip_slots.append({int(equip["equip_slot"]) : int(equip["item_category"])})
+
+		
--- Scenes/UI/Inventory/DragItems.gd
+++ Scenes/UI/Inventory/DragItems.gd
@@ -12,7 +12,7 @@ func get_drag_data(position: Vector2):
 	data["origin_texture"] = texture
 	data["origin_texture_file_name"] = texture.get_path().split("Items/")[1]
 	data["origin_item_id"] = ItemDatabase.GetItemID(data["origin_texture_file_name"])
-	data["origin_slot_number"] = get_parent().name.split("slot")[1]
+	data["origin_slot_number"] = int(get_parent().name.split("slot")[1])
 	data["origin_parent"] = get_parent().get_name()
 	data["origin_item_category_id"] = ItemDatabase.GetItemCategory(data["origin_texture_file_name"])
 
@@ -37,10 +37,10 @@ func can_drop_data(position: Vector2, data) -> bool:
 		data["destination_texture"] = texture
 		data["destination_texture_file_name"] = texture.get_path().split("Items/")[1]
 		data["destination_item_id"] = ItemDatabase.GetItemID(data["destination_texture_file_name"])
-		data["destination_slot_number"] = get_parent().get_name().split("slot")[1]
+		data["destination_slot_number"] = int(get_parent().get_name().split("slot")[1])
 		data["destination_parent"] = get_parent().get_name()
 		data["destination_category_id"] = ItemDatabase.GetItemCategory(data["destination_texture_file_name"])
-	
+
 		if AllowSwitch(data["origin_slot_number"], data["origin_item_category_id"], data["destination_slot_number"], data["destination_category_id"]):
 			return true
 	return false
@@ -51,45 +51,25 @@ func drop_data(position: Vector2, data) -> void:
 	texture = data["origin_texture"]
 	data["origin_node"].texture = data["destination_texture"]
 	
-	
-	#checks
-#	if name[0] != "b":
-##		if ValidSlot(data["origin_item_id"], )
-#		texture = data["origin_texture"]
-
 
 func AllowSwitch(origin_item_slot, origin_item_category_id, destination_item_slot, destination_item_category_id):
 	var allowed_to_switch = false
-	if int(origin_item_slot) and int(destination_item_slot) <= 25:
+	if int(origin_item_slot) <=25 and int(destination_item_slot) <= 25:
+		print("both under 25")
 		return true
 	elif ItemAllowedInSlot(destination_item_slot, origin_item_category_id):
+		print("origin item can go to destination slot")
 		if ItemAllowedInSlot(origin_item_slot, destination_item_category_id):
+			print("destination item can go to origin slot")
 			return true
 	return false 
  
 func ItemAllowedInSlot(item_slot, item_category_id):
-	item_slot = int(item_slot)
-	item_category_id = int(item_category_id)
+	var dict : Dictionary = {item_slot : item_category_id}
 	if item_slot <= 25:
 		return true
-	elif item_slot == 26 and item_category_id == 1:
-		return true
-	elif item_slot == 27 and item_category_id == 2:
-		return true
-	elif item_slot == 28 and item_category_id == 9:
-		return true
-	elif item_slot == 29 and item_category_id == 6:
-		return true
-	elif item_slot == 30 and item_category_id == 7:
-		return true
-	elif item_slot == 31 and item_category_id == 3:
-		return true
-	elif item_slot == 32 and item_category_id == 8:
-		return true
-	elif item_slot == 33 and item_category_id == 8:
-		return true
-	elif item_slot == 34 and item_category_id == 4:
-		return true
-	elif item_slot == 35 and item_category_id == 5:
-		return true
+	else:
+		for row in ItemDatabase.equip_slots:
+			if row.hash() == dict.hash():
+				return true
 	return false
