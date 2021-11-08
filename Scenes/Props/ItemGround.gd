cff03649fed4adf8c0febd25d896e9f35c0da35c
Rick Willcox <bigshakagames@gmail.com>
2021-11-09 00:06:18 +1000

    added item fling towards backpack area

--- Scenes/Props/ItemGround.gd
+++ Scenes/Props/ItemGround.gd
@@ -9,6 +9,8 @@ var time_left_for_nonkiller_pickup : float = 5.0
 #move to common submodule eventually
 var pick_up_range : int = 100
 
+onready var tween = get_node("Tween")
+
 func _ready():
 	$Sprite.texture = item_texture
 	$PickupTimer.wait_time = time_left_for_nonkiller_pickup
@@ -25,7 +27,13 @@ func _on_PickupTimer_timeout() -> void:
 
 
 func _on_Button_pressed():
+	var tween_position : Vector2 = get_node("../../Player").position + Vector2(386, 200)
 	if $Sprite.modulate.a == 1 and get_node("../../Player").position.distance_to(self.position) < pick_up_range:
 		emit_signal("pickup", item_id, name)
 		# TODO: maybe only cleanup the item if server okayed it
-		queue_free()
+		tween.interpolate_property(self, "position", position, tween_position, 1.0, Tween.TRANS_BACK, Tween.EASE_IN)
+		tween.start()
+
+
+func _on_Tween_tween_all_completed() -> void:
+	queue_free()
