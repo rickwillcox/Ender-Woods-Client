0699dc08330c65c9b27e4820624e167c5de349ba
Mkneeshaw <Michael_Kneeshaw@yahoo.com>
2021-11-10 16:51:53 -0500

    selector init

--- Scenes/UI/MenuSelector/MenuSelector.gd
+++ Scenes/UI/MenuSelector/MenuSelector.gd
@@ -5,7 +5,16 @@ export var current_menu : int = 0
 onready var menu_buttons = [$InventoryBtn, $StatusBtn]
 
 func _ready():
-	menu_buttons[current_menu].pressed = true
+	init_menu_buttons_pressed()
+
+func init_menu_buttons_pressed():
+	var index = 0
+	for btn in menu_buttons:
+		if index == current_menu:
+			btn.pressed = true
+		else:
+			btn.pressed = false
+		index += 1
 
 func _on_InventoryBtn_toggled(button_pressed):
 	if current_menu == 0:
