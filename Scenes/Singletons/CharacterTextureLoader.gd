53f2a595e942bc89f0a636253a204db94b34dd8d
Rick Willcox <bigshakagames@gmail.com>
2021-11-14 15:57:13 +1000

    changed how chat text loader works

--- Scenes/Singletons/CharacterTextureLoader.gd
+++ Scenes/Singletons/CharacterTextureLoader.gd
@@ -1,19 +1,26 @@
 extends Node
 
-var item_id_to_asset = {
-	1 : "res://Assets/Character/items/silver_helmet_spritesheet.png",
-	2 : "res://Assets/Character/items/silver_breastplate_spritesheet.png",
-	3 : "res://Assets/Character/items/silver_gloves_spritesheet.png",
-	4 : "res://Assets/Character/items/silver_pants_spritesheet.png",
-	5 : "res://Assets/Character/items/silver_boots_spritesheets.png",
-}
-
-var item_textures : Dictionary
+var dir : Directory = Directory.new()
+var item_id_to_asset : Dictionary = {}
+var item_textures : Dictionary 
 
 func _ready():
 	for key in item_id_to_asset:
 		item_textures[key] = load(item_id_to_asset[key])
+	
+	dir.open("res://Assets/Character/items/")
+	dir.list_dir_begin(true, true)
+	#get all files that end in .png from the directory above
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif file.ends_with(".png"):
+			# First part of the file name is item_id
+			var id = int(file.split("_")[0])
+			item_id_to_asset[id] =  load("res://Assets/Character/items/" + file)
 
+	
 func get_item_texture(item_id):
 	if item_textures.has(item_id):
 		return item_textures[item_id]
