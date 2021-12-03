981b33f995fb614d6c89d0a50cf22363eca9b717
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-12-03 12:36:38 +0100

    Tiled importer and test tilemap
    Added modified tiled importer plugin (it ignores ground tiles).
    Added a tiled project for the tilemap.

---
+++ map/beach/tropical shores, just trees.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tropical shores, just trees" tilewidth="80" tileheight="112" tilecount="12" columns="6">
+ <image source="tropical shores, just trees.png" width="480" height="224"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23.7835" y="102.039"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ map/beach/tropical shores.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="tropical shores" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="tropical shores.png" width="256" height="256"/>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="300"/>
+   <frame tileid="39" duration="300"/>
+   <frame tileid="71" duration="300"/>
+   <frame tileid="103" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="40" duration="300"/>
+   <frame tileid="72" duration="300"/>
+   <frame tileid="104" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="300"/>
+   <frame tileid="55" duration="300"/>
+   <frame tileid="87" duration="300"/>
+   <frame tileid="119" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="300"/>
+   <frame tileid="56" duration="300"/>
+   <frame tileid="88" duration="300"/>
+   <frame tileid="120" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
