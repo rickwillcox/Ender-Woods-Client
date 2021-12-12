cce9074fd68db33299046b6c8422b6be689768d3
Rick Willcox <bigshakagames@gmail.com>
2021-12-12 11:58:38 +1000

    this commit was due to importing new textures

---
+++ Tiled-Map-Files-New/tilesets-tsx/Houses.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Houses" tilewidth="224" tileheight="224" tilecount="24" columns="8">
+ <image source="../tilesets-png/Houses.png" width="1962" height="895"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="4" y="0" width="216" height="224"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="71" y="96" width="96" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="39" y="112" width="144" height="112"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="32" width="160" height="192"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="48" width="176" height="176"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Tiled-Map-Files-New/tilesets-tsx/monsters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="monsters" tilewidth="48" tileheight="48" tilecount="40" columns="10">
+ <image source="../tilesets-png/monsters.png" width="512" height="193"/>
+</tileset>
